c95bbe8b86bee6bb8c8ec34dcc06c484e8b280c3
zellduck <toandvu90@gmail.com>
2015-05-05 09:23:21 +0700

    done practice 1

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
-<?php
+﻿<?php
 /*
  * Bài tập 1:
  * Khai báo biến tên a và gán giá trị là 10 và có kiểu dữ liệu là số nguyên
  */
-
+$a = 10;
 /*
  * Bài tập 2:
  * Khai báo hằng  tên a và gán giá trị là 10 và có kiểu dữ liệu là số nguyên
