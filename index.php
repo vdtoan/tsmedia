7ba6311c20c5c41c502754cd8d8210b887a2fc2a
hongtuyet <hongtuyet@ioi.vn>
2015-05-06 19:01:07 +0700

    hoan tat bai tap 1

--- index.php
+++ index.php
@@ -11,12 +11,32 @@
  * Phân tích các biến số và hăng số,
  * Khởi tạo và gán giá trị tương ứng với đề bài 
  */
+$nhan_vien = 3 ;
+$nhan_vien = 30;
+$nhan_vien = 100;
+$nam = 1;
+$nam = 2;
+$nam = 4;
+$co_phan;
+$hinh_thuc_cong_ty = "tnhh";
+$hinh_thuc_cong_ty = "co_phan";
+define('VON_BAN_DAU',10000);
 
+ 
 /*
  * Bài tập 2:
  * Giả sử, nếu sau 1 năm, công ty phá sản và giải thể thì tất cả nhân sự đều bị sa thải,
  * số lương nhân viên và giám đốc sẽ bằng 0
  * Yêu cầu : hiển thị số lượng nhân sự sau khi phá sản
+ */
+                            
+if($nam == 1) {
+	$nhan_vien = 0;
+	$giam_doc = 0;
+}
+ 
+ 
+ 
  */
 
 /*
@@ -26,6 +46,9 @@
  * Yêu cầu : hiển thị tên doanh nghiệp cùng hình thức doanh nghiệp và số lượng nhân viên
  */
 
+	if ($nam == 1 && $nhan_vien > 30) {
+		$cong_ty = 'co_phan';
+	}
 /*
  * Bài tập 4:
  * Nếu sau 4 năm kể từ lúc chuyển đổi cổ phần , số lượng nhân viên nhiều hơn hoặc bằng 20,
@@ -34,3 +57,9 @@
  * Yêu cầu : Nếu doanh nghiệp bị giải thể, thì hiển thị tên doanh nghiẹp cùng với thông điệp giải thể,
  *           Nếu không giải thể thì hiển thị tất cả thông tin về doanh nghiệp.
  */
+ 
+	if ($nam > 4 && $hinh_thuc_cong_ty == 'co_phan' && $nhan_vien >= 20 && $von < 20000 || $co_dong == 1) {
+		$cong_ty = 'giai the ';
+	}
+	
+
